1c5c701b2f3c9ad69914b70abc9fbbcd621f5f99
Manuel Sousa <mlrcbsousa@gmail.com>
2019-02-13 10:24:41 +0100

    jwt with devise

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -7,7 +7,8 @@ class ApiController < ApplicationController
   # avoids repeated requests to the server
   skip_before_action :verify_authenticity_token
 
-  before_action :authenticate_token!
+  # before_action :authenticate_token!
+  before_action :authenticate_user!
   before_action :set_default_format
 
   private
@@ -16,17 +17,17 @@ class ApiController < ApplicationController
     request.format = :json
   end
 
-  def authenticate_token!
-    payload = JsonWebToken.decode(auth_token)
-    @current_user = User.find(payload['sub'])
-    p @current_user
-  rescue JWT::ExpiredSignature
-    render json: { errors: ['Auth token has expired'] }, status: :unauthorized
-  rescue JWT::DecodeError
-    render json: { errors: ['Invalid auth token'] }, status: :unauthorized
-  end
+  # def authenticate_token!
+  #   payload = JsonWebToken.decode(auth_token)
+  #   @current_user = User.find(payload['sub'])
+  #   p @current_user
+  # rescue JWT::ExpiredSignature
+  #   render json: { errors: ['Auth token has expired'] }, status: :unauthorized
+  # rescue JWT::DecodeError
+  #   render json: { errors: ['Invalid auth token'] }, status: :unauthorized
+  # end
 
-  def auth_token
-    @auth_token ||= request.headers.fetch('Authorization', '').split(' ').last
-  end
+  # def auth_token
+  #   @auth_token ||= request.headers.fetch('Authorization', '').split(' ').last
+  # end
 end
